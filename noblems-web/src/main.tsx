f61a5219c8507cd7fb43ee5074dafd89afc7278e
wufwufflez <noblems.wuffle@gmail.com>
2026-04-13 23:40:08 +0700

    Update main.tsx

--- noblems-web/src/main.tsx
+++ noblems-web/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom"; // ✅ IMPORTANT
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ THIS FIXES YOUR ERROR */}
+    <BrowserRouter basename="/NMSFrontend/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
