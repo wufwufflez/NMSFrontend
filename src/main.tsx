24ef24d4e2bc13f57bfeee5cd85d8ba85426309f
Yan Tianlu <122171664+ytl41@users.noreply.github.com>
2026-04-15 01:53:01 +0700

    change root

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"
 import { BrowserRouter } from "react-router-dom"; // ✅ IMPORTANT
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/NMSFrontend/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
