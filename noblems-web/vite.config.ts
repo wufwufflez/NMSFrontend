3c12d6252399d54cfcca0b7563b545ede6862f15
wufwufflez <noblems.wuffle@gmail.com>
2026-04-15 00:43:19 +0700

    Update vite.config.ts

--- noblems-web/vite.config.ts
+++ noblems-web/vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NMSFrontend/',
+  base: '/',
   plugins: [
     tailwindcss(),
   ],
